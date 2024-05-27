2d32d4f8a0df195d4bf6d88c5c9ec5d8bedd5afb
thoriqarr21 <amuhammadthoriq@gmail.com>
2024-05-27 13:59:32 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5003,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
